0047b128b9e29455cf3996b32a42be51b8594b8f
fab3F <96261710+fab3F@users.noreply.github.com>
2026-08-15 02:35:56 +0200

    small fixes

--- components/error.js
+++ components/error.js
@@ -3,7 +3,7 @@ import { browser } from 'wxt/browser';
 document.title = browser.i18n.getMessage("errorTitle");
 document.querySelectorAll('[data-i18n]').forEach(element => {
   const key = element.getAttribute('data-i18n');
-  element.innerHTML = browser.i18n.getMessage(key);
+  element.textContent = browser.i18n.getMessage(key);
   element.style.visibility = 'visible';
 });
 
--- wxt.config.ts
+++ wxt.config.ts
@@ -4,20 +4,19 @@ export default defineConfig({
   modules: ['@wxt-dev/auto-icons'],
   manifest: {
     name: "RickRollProtector",
-    author: {
-      email: "fab3F@programmer.net"
-    },
     developer: {
       name: "fab3F",
       url: "https://fab3F.github.io"
     },
-    version: "2.0.0.0",
     default_locale: "en",
     description: "__MSG_manifestDesc__",
     permissions: ["storage"],
     browser_specific_settings: {
       gecko: {
-        id: "rickrollprotector@fab3F.github.io"
+        id: "rickrollprotector@fab3F.github.io",
+        data_collection_permissions: {
+          required: ["none"],
+        },
       }
     },
     web_accessible_resources: [{
